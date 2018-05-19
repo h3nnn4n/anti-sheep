137ddf96f0f165610016e049518484e4bf9e60ad
h3nnn4n <uber.renan@gmail.com>
2018-05-18 23:18:26 -0300

    Update on project files + defs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod defs;
+
 fn main() {
-    println!("Hello, world!");
+    println!(
+        "Hello, world! {} {} {}",
+        defs::N_MOVE,
+        defs::N_TWIST,
+        defs::N_CORNERS
+    );
 }
