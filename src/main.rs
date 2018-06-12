a858d3b2d235f0d5e0312a8e9508b010abda448b
h3nnn4n <uber.renan@gmail.com>
2018-06-12 20:27:48 -0300

    Reverted main to the old behaviour

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,16 @@ fn main() {
 
     //c.from_string(cubestring);
 
-    for _ in 0..1000 {
-        c.random_shuffle(30);
+    c.random_shuffle(30);
 
-        let t_start = Instant::now();
-        let solve_sequence = c.solve();
-        let t_end = Instant::now();
+    let t_start = Instant::now();
+    let solve_sequence = c.solve();
+    let t_end = Instant::now();
 
-        let diff = t_end.duration_since(t_start);
-        println!("{:?}.{:?}", diff.as_secs(), diff.subsec_nanos());
+    let diff = t_end.duration_since(t_start);
+    println!("{:?}.{:?}", diff.as_secs(), diff.subsec_nanos());
 
-        c.do_move_sequence(solve_sequence);
+    c.do_move_sequence(solve_sequence);
 
-        assert!(c.is_solved());
-    }
+    assert!(c.is_solved());
 }
